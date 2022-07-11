81a17bc11a668c6a3bf3fee117003b27008e3652
Akshansh Chaudhary <akshansh.pro.007@gmail.com>
2022-07-11 18:44:53 +0530

    Add files via upload

---
+++ react/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Parent from './Parent';
+
+function App() {
+  return (
+    <div className="App">
+       
+        <Parent Name1='Nord Stage 3 88 Keys' price1= '$5738' Name2='Line 6 Helix Rack guitar Processor - 1x opened box
+        ' price2= '$1831' Name3='Nord lead A1' price3= '$1700' Name4='Moog Subsequent 37' price4= '$1700' Name5='Moog Miniataur 2010s black' price5= '$682' />
+        
+    </div>
+     );
+}
+
+export default App;
